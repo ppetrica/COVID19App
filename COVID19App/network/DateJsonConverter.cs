eb93583a9357a7a273f886e0b71780c6c28a8b22
Marin Moisii <moisii.marin@gmail.com>
2020-03-31 22:06:52 +0300

    Added class for custom deserialization of data value.

---
+++ COVID19App/network/DateJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using core;
+using Newtonsoft.Json;
+using System;
+
+
+namespace network
+{
+    /// <summary>
+    /// This class is used to deserialize date value from json to core.Date structure.
+    /// </summary>
+    class DateJsonConverter : JsonConverter<Date>
+    {
+        public override Date ReadJson(JsonReader reader, Type objectType, Date existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return Date.Parse((string)reader.Value);
+        }
+
+        public override void WriteJson(JsonWriter writer, Date value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
